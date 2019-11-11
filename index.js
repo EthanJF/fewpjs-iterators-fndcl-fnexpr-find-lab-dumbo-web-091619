fb8d1ce9163fd9bbf7705a7832f687dfa7e68ffb
EthanJF <ethanjosephfournier@gmail.com>
2019-11-10 19:05:51 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let winningRecord = array.find(({ result }) => result === 'W')
+  if(winningRecord){
+    return winningRecord["year"]
+  }
+}
